5b8f03a4842b35e76119fac450d9bd0d2cde8188
oscarhluviano <oscarhluviano@gmail.com>
2022-03-31 23:41:55 -0600

    Se mejoró la vista de lo datos y se añadieron correcciones

--- Practica3/ViewController.swift
+++ Practica3/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var datos = [String:Any]()
+    
+    @IBOutlet weak var imageV: UIImageView!
+    @IBOutlet weak var textV: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let nombre = datos["name"] as? String
+        let ingredientes = datos["ingredients"] as? String
+        let instruc = datos["directions"] as? String
+        let img = UIImage(named: (datos["image"] as? String)!) ?? UIImage()
+        
+        
+        
+        textV.text = "Nombre: \(nombre!)\n \n" +
+        "Ingredientes: \(ingredientes!)\n \n"
+        + "Instrucciones: \(instruc!)"
+        imageV.image = img
+        
+        
     }
 
 
